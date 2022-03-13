929b8d8aeb20a7e15e9bc759751c84f382d55308
Miguel Pazo <miguel.ps19@gmail.com>
2022-03-13 21:06:57 +0100

    improve grahics

--- app.R
+++ app.R
@@ -16,12 +16,15 @@
 # install.packages("tidyverse")
 # install.packages("DT")
 # install.packages("ggplot2")
+# install.packages("randomcoloR")
 
 library(shinydashboard)
 library(dplyr)
 library(tidyverse)
 library(DT)
 library(ggplot2)
+library(randomcoloR)
+library(scales)
 
 
 # Carga de dataset
@@ -50,8 +53,18 @@ data <- transform(data, sup_suelo_media  = as.integer(sup_suelo_media))
 data <- transform(data, val_cat_barrio = as.double(val_cat_barrio))
 data <- transform(data, val_cat_medio = as.double(val_cat_medio))
 
-data 
 
+# Asignando color por barrio
+dataBarrios <- data %>%
+  distinct(barrio_cod)
+
+pal <- distinctColorPalette(nrow(dataBarrios))
+dataBarrios$barrio_color <- pal 
+
+data <- merge(data, dataBarrios, by="barrio_cod")
+
+
+# Filtros del sidebar
 stUso <- data %>%
   group_by(uso_deno) %>%
   slice(1) %>%
@@ -67,7 +80,7 @@ vDistrictos <- as.vector(stDistritos$distrito_nombre)
 
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Grupo N"),
+  dashboardHeader(title = "Grupo 6"),
   dashboardSidebar(
     selectInput(
       inputId = "uso",
@@ -105,6 +118,7 @@ ui <- dashboardPage(
 server <- function(input, output) {
   observe({
     print(input$distrito)
+    print(input$uso)
   })
   
   output$inmueblesBario <- renderInfoBox({
@@ -115,6 +129,8 @@ server <- function(input, output) {
         filter(data$uso_deno == input$uso & data$distrito_nombre == input$distrito) %>%
         summarise(total = sum(inmuebles_barrio))
     }
+    
+    data2 <- format(round(as.numeric(data2), 1), nsmall=1, big.mark=",")
   
     infoBox("Inmuebles por barrio", 
             data2, 
@@ -147,13 +163,13 @@ server <- function(input, output) {
     if (!is.null(input$distrito)) {
       data2 <- data %>%
         filter(data$uso_deno == input$uso & data$distrito_nombre == input$distrito) %>%
-        summarise(total = mean(val_cat_barrio, na.rm=TRUE))
+        summarise(total = mean(val_cat_medio, na.rm=TRUE))
       
       data2 <- round(data2, digits = 2)
     }
     
-    infoBox("Valor medio", 
-            paste0("€ ", format(round(data2/1e6, 1), trim=TRUE), "M"), 
+    infoBox("Valor medio en miles de €", 
+            paste0("€ ", format(round(data2/1e3, 1), trim=TRUE), "M"), 
             icon = icon("credit-card"),
             color = "purple"
     )
@@ -165,8 +181,18 @@ server <- function(input, output) {
       filter(data$uso_deno == input$uso & data$distrito_nombre == input$distrito) %>%
       arrange(desc(val_cat_medio))
     
-    grafica <- ggplot(data=data2, mapping = aes_string(data2$barrio_cod, data2$val_cat_medio)) + 
-      geom_point(mapping = aes_string(color = data2$barrio_cod), size=data2$inmuebles_barrio/100) + 
+    maxValue <- max(data2$inmuebles_barrio, na.rm = TRUE)
+    divisor <- 100
+    
+    if(maxValue < 1000) {
+      divisor <- 20
+    }
+    
+    grafica <- ggplot(data=data2, mapping = aes(barrio_nombre, val_cat_medio)) + 
+      geom_point(mapping = aes(color = barrio_color), size=data2$inmuebles_barrio/divisor) + 
+      geom_text(aes(label=format(as.numeric(inmuebles_barrio), 0, nsmall=0, big.mark=",")), position=position_dodge(width=0.9), vjust=0.3, hjust=1.5) +
+      scale_color_identity() +
+      scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-3)) +
       xlab("Códigos de Barrios") +
       ylab("Valor medio catastral") +
       labs(title="Valor / Número de inmuebles por barrio", subtitle="El tamaño de los puntos indica el número de inmuebles por barrio") +
@@ -182,14 +208,15 @@ server <- function(input, output) {
       arrange(desc(anio_cons_medio))
     
     grafica <- ggplot(data=data2, aes(x=reorder(barrio_nombre, anio_cons_medio), y=anio_cons_medio)) +
-      geom_bar(stat="identity", width=0.5, fill="steelblue") +
+      geom_bar(stat="identity", width=0.5, fill=data2$barrio_color) +
       geom_text(aes(label=paste0(anio_cons_medio)), position=position_dodge(width=0.9), vjust=0.3, hjust=1.5) +
       coord_flip() +
       ylab("Año") +
       xlab("Barrios") +
       ggtitle("Año medio de construcción por barrio")
     
-    grafica  })
+    grafica  
+  })
   
   # Tabla
   output$tablaDatos <- renderDT({
