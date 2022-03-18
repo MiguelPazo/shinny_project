4b540a8c5fb641de644247d6988f399e85f07281
Miguel Pazo <miguel.ps19@gmail.com>
2022-03-18 17:00:10 +0100

    update comments

--- app.R
+++ app.R
@@ -1,15 +1,6 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-# Configurar working directory
-# setwd("D:\__ProjectsDev\EAE\06_shinny_grupal\App")
-
+#############################
+# Librerias a Instalar
+#############################
 # Instalar librerias
 # install.packages("shinydashboard")
 # install.packages("dplyr")
